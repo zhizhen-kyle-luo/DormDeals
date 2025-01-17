629e878d00dda532d128989becba7ac03a76385a
anthony zheng <zhenganthony26@gmail.com>
2025-01-16 23:44:57 -0500

    changed user model

--- skeleton-main/server/models/user.js
+++ skeleton-main/server/models/user.js
@@ -3,6 +3,9 @@ const mongoose = require("mongoose");
 const UserSchema = new mongoose.Schema({
   name: String,
   googleid: String,
+  picture: Image,
+  description: String,
+  email: String,
 });
 
 // compile model from schema
