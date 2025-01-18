de557b1218b1281693769fc0b5c78e6bb67b616d
zhizhen-kyle-luo <zhizhen.luo07@gmail.com>
2025-01-18 13:05:18 -0500

    added home page

---
+++ skeleton-main/client/src/components/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+
+const Home = (props) => {
+  return (
+    <div className="Home-container">
+      <h1>Welcome to Your Home Page</h1>
+      {/* Add your home page content here */}
+    </div>
+  );
+};
+
+export default Home; 
