e32a19fba5579585980e8c4b1022382d112161a0
anthony zheng <zhenganthony26@gmail.com>
2025-01-21 15:17:30 -0500

    replaced profile and shopping cart icons with ones with transparent background

--- skeleton-main/client/src/components/modules/NavBar.jsx
+++ skeleton-main/client/src/components/modules/NavBar.jsx
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom";
 import "./NavBar.css";
 // Import images
 import newOrderIcon from "../../assets/plus_sign.png";
-import cartIcon from "../../assets/shopping_cart.webp";
-import profileIcon from "../../assets/profile.jpg";
+import cartIcon from "../../assets/shopping_cart.png";
+import profileIcon from "../../assets/profile.webp";
 
 const NavBar = (props) => {
   return (
@@ -16,13 +16,13 @@ const NavBar = (props) => {
       </div>
       <div className="NavBar-linkContainer">
         <Link to={`/profile/${props.userId}`} className="NavBar-link" title="Profile">
-          <img src={profileIcon} alt="Profile" className="Profile-icon"/>
+          <img src={profileIcon} alt="Profile" className="Profile-icon" />
         </Link>
         <Link to={`/NewPage/${props.userId}`} className="NavBar-link" title="New Order">
-          <img src={newOrderIcon} alt="New Order" className="New-order-icon"/>
+          <img src={newOrderIcon} alt="New Order" className="New-order-icon" />
         </Link>
         <Link to={`/Cart/${props.userId}`} className="NavBar-link" title="Shopping Cart">
-          <img src={cartIcon} alt="Shopping Cart" className="Cart-icon"/>
+          <img src={cartIcon} alt="Shopping Cart" className="Cart-icon" />
         </Link>
       </div>
     </nav>
