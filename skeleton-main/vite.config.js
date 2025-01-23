70a8dceb30ae9bdbb5c470772bdfe78261abcaa2
techeiFatima <fatima12@mit.edu>
2025-01-23 18:01:01 -0500

    Fix socket.io configuration and update dependencies

--- skeleton-main/vite.config.js
+++ skeleton-main/vite.config.js
@@ -19,8 +19,8 @@ export default defineConfig({
       '/socket.io': {
         target: 'http://localhost:3000',
         ws: true,
-        changeOrigin: true,
-    },
+        changeOrigin: true
+      }
     }
   }
 });
