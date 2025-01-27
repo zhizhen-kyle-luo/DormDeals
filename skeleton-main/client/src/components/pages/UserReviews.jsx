08aae51e9096e2df6f9bfb08aaeb74e88c21840a
anthony zheng <zhenganthony26@gmail.com>
2025-01-27 16:47:45 -0500

    user reviews style

--- skeleton-main/client/src/components/pages/UserReviews.jsx
+++ skeleton-main/client/src/components/pages/UserReviews.jsx
@@ -7,26 +7,48 @@ import "./UserReviews.css";
 
 const UserReviews = () => {
   let props = useParams();
-  const [seller, setSeller] = useState("");
+  const [seller, setSeller] = useState({});
   const [reviews, setReviews] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const ratingIcon = {
+    1: "★☆☆☆☆",
+    2: "★★☆☆☆",
+    3: "★★★☆☆",
+    4: "★★★★☆",
+    5: "★★★★★",
+  };
 
   useEffect(() => {
+    setIsLoading(true);
     get(`/api/user`, { userid: props.userId, picture: defaultpfpimg }).then((sellerObj) => {
-      setSeller(sellerObj[0].name);
+      setSeller(sellerObj[1]);
       get(`/api/reviews`, { name: sellerObj[0].name, _id: props.userId }).then((reviewsObj) => {
         setReviews(reviewsObj);
+        setIsLoading(false);
       });
     });
   }, []);
 
+  if (isLoading) {
+    return <h3>Loading...</h3>;
+  }
+
   return (
     <div className="Reviews-container">
-      <h1>Reviews for {seller}</h1>
+      <h1>Reviews for {seller.user.name}</h1>
+      <div className="User-rating">{`★ ${seller.rating[0]} (${seller.rating[1]})`}</div>
       <div className="Reviews-grid">
         {reviews.map((review) => (
           <div className="Review-card">
-            <h3 className="Review-author">{review.reviewer.name}</h3>
-            <div className="Review-rating">Rating: {review.rating}</div>
+            <div className="Review-author">
+              <div className="Review-author-imageContainer">
+                <img src={seller.picture} className="Review-author-image" />
+              </div>
+              <h3 className="Review-author-name">{review.reviewer.name}</h3>
+            </div>
+            <div className="Review-rating">{ratingIcon[review.rating]}</div>
+            <hr className="Review-divide" />
             <div className="Review-main">{review.review}</div>
           </div>
         ))}
